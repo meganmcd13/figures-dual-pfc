425def08469684306b9439908bddabda46597b49
meganmcd13 <mmcdonne@andrew.cmu.edu>
2025-03-21 22:14:11 -0400

    Update dual_pfc_funcs.py

--- dual_pfc_funcs.py
+++ dual_pfc_funcs.py
@@ -1,65 +1,34 @@
-# utilities
+# MODULE OF HELPER FUNCTIONS
 
-def jitter(length=1):
+def getParams():
+    # returns dictionary of helpful parameters for plotting 
     import numpy as np
-    spacing = 0.2
-    return np.random.uniform(low=-spacing,high=spacing,size=length)
 
-# compute within area rsc for full (multiarea) covariance matrix
-def compute_rsc_within_pccafa(Sigma_full, xDim):
-    import numpy as np
-    # Sigma is xDim+yDim x xDim+yDim
-    Sigma_x = Sigma_full[:xDim,:xDim]
-    Sigma_y = Sigma_full[xDim:,xDim:]
-    rscs_x, rscs_y = np.array([]), np.array([])
+    # plot colors
+    color_map = {
+        'across':np.array([255,76,178])/255, # pink
+        'within1':np.array([111,192,255])/255, # light blue - right hemisphere
+        'within2':np.array([0,87,154])/255, # dark blue - left hemisphere
+        'within':np.array([0,144,255])/255, # medium blue - collapsed across both hemispheres
+        'independent':np.array([200,200,200])/255 # gray
+    }
+    params = {
+        'subjects': ('pepe','wakko','satchel'),
+        'markers': ('o','s','^'), # corresponding plot markers
+        'color_map': color_map
+    }
+    return params
 
-    i_s,j_s = np.tril_indices(xDim, -1)
-    for i,j in zip(i_s,j_s):
-        sig_ij = Sigma_x[i,j]
-        sig_ii = Sigma_x[i,i]
-        sig_jj = Sigma_x[j,j]
-        rsc = sig_ij / np.sqrt(sig_ii * sig_jj)
-        rscs_x = np.append(rscs_x, rsc)
-        
-    i_s,j_s = np.tril_indices(xDim, -1)
-    for i,j in zip(i_s,j_s):
-        sig_ij = Sigma_y[i,j]
-        sig_ii = Sigma_y[i,i]
-        sig_jj = Sigma_y[j,j]
-        rsc = sig_ij / np.sqrt(sig_ii * sig_jj)
-        rscs_y = np.append(rscs_y, rsc)
-    return rscs_x, rscs_y
-
-# compute across area rsc for full (multiarea) covariance matrix
-def compute_rsc_across_pccafa(Sigma_full,xDim):
-    import numpy as np
-    # Sigma is xDim+yDim x xDim+yDim
-    yDim = Sigma_full.shape[0] - xDim
-    Sigma = Sigma_full[xDim:,:xDim] # get neurons from opposite regions
-    Sigma_x = Sigma_full[:xDim,:xDim]
-    Sigma_y = Sigma_full[xDim:,xDim:]
-    rscs = np.array([])
-    i_s,j_s = np.indices((yDim,xDim))
-    for i,j in zip(i_s,j_s):
-        sig_ij = Sigma[i,j]
-        sig_ii = Sigma_x[i,i]
-        sig_jj = Sigma_y[j,j]
-        rsc = sig_ij / np.sqrt(sig_ii * sig_jj)
-        rscs = np.append(rscs, rsc)
-    return rscs
-
-# plot a raster on given ax
-def plot_raster(X,ax):
-    import numpy as np
-    # assumes you have already called subplot on the figure that you want
-    # X: (num_neurons, num_timepoints)
-    X = np.flipud(X)
-    num_neurons = X.shape[0]
-    num_timepoints = X.shape[1]
-    for i_neuron in range(num_neurons):
-        for i_time in range(num_timepoints):
-            if X[i_neuron,i_time] == 1:
-                ax.plot([i_time,i_time], [i_neuron,i_neuron+1],'-k',linewidth=0.3)
+def getBaseSimParams():
+    # return dictionary of base simulation parameters
+    params = {
+        'n_trials' : 1000,
+        'xDim' : 30, 'yDim' : 30,
+        'zDim' : 5, 'zxDim' : 3, 'zyDim' : 3,
+        'n_boots' : 100,
+        'sv_goal' : (15,10),
+    }
+    return params
 
 # load dictionary using pickle
 def load_dict(filename):
@@ -69,78 +38,37 @@ def load_dict(filename):
     # reconstructing the data as dictionary
     return pickle.loads(data)
 
-# flatten a list of lists:
-def flatten(xs):
-    from collections.abc import Iterable
-    for x in xs:
-        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
-            yield from flatten(x)
-        else:
-            yield x
+# save dictionary using pickle
+def save_dict(obj, filename):
+    # obj: dictionary to be saved
+    # filename: name to save file under
+    import pickle
+    if '.pkl' in filename: save_name = filename
+    else: save_name = filename + '.pkl'
 
-# extract pCCA-FA model parameters
-def extract_mdl_params(fit_dat):
-    tmp = fit_dat['params'].item()
-    param_dict={x:tmp[x].item() for x,y in tmp.dtype.fields.items()}
-    param_dict['mu_x'] = param_dict['mu_x'].flatten()
-    param_dict['mu_y'] = param_dict['mu_y'].flatten()
-    param_dict['psi_x'] = param_dict['psi_x'].flatten()
-    param_dict['psi_y'] = param_dict['psi_y'].flatten()
-    param_dict['zDim'] = int(param_dict['zDim'])
-    param_dict['zxDim'] = int(param_dict['zxDim'])
-    param_dict['zyDim'] = int(param_dict['zyDim'])
-    return param_dict
+    with open(save_name, 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-# preprocess spike counts to remove condition means and auto regressive prediction
-def preprocess_counts(counts,targ_angs,binsize,ar_order):
-    import counts_pkg.counts_analysis as cp
-    sc_obj1 = cp.counts_analysis(counts,targ_angs,binsize)
-    cond_means = sc_obj1.compute_cond_means()
-    sc_obj2 = cp.counts_analysis(sc_obj1.rm_cond_means(),targ_angs,binsize)
-    _,ar_est = sc_obj2.rm_autoreg(order=ar_order,auto_type='mean',fa_remove=True,fa_dims=15)
-    return sc_obj1.rm_cond_means(), ar_est, cond_means
-
-# function for plotting figure 5
-def plot_metric(dat,sub,ax,col,sym,metric='psv'):
+# generate random jitter for plotting clarity
+def jitter(length=1,spacing=0.2,rand_seed=None):
     import numpy as np
-    import pcca_fa_mdl as pf
-    if sym=='o':
-        jit = -0.1
-    elif sym=='s':
-        jit = 0
-    else:
-        jit = 0.1
-    # loop through each session
-    sess_names = [k for k,v in dat.items() if k.lower().startswith(sub)]
-    psv_x,psv_y = np.zeros(len(sess_names)),np.zeros(len(sess_names))
-    psv_priv_x,psv_priv_y = np.zeros(len(sess_names)),np.zeros(len(sess_names))
-    for i,sess in enumerate(sess_names):
-        mdl_params = extract_mdl_params(dat[sess])            
-        pf_mdl = pf.pcca_fa()
-        pf_mdl.set_params(mdl_params)
-        fit_metrics = pf_mdl.compute_metrics()[metric]
-        psv_x[i],psv_y[i] = fit_metrics[metric+'_x'],fit_metrics[metric+'_y']
-        psv_priv_x[i],psv_priv_y[i] = fit_metrics[metric+'_priv_x'],fit_metrics[metric+'_priv_y']
-    psv_within = np.concatenate((psv_priv_x,psv_priv_y))
-    psv_across = np.concatenate((psv_x,psv_y))
-    if metric=='dshared':
-        count_dict = dict()
-        for i in range(len(psv_within)):
-            tmp = (psv_within[i],psv_across[i])
-            if tmp not in count_dict:
-                count_dict[tmp] = 1
-            else:
-                count_dict[tmp] += 1
-        for t in count_dict:
-            ax.plot(t[0]+jit,t[1]+jit,marker=sym,ls='',color=col,markersize=2+1.5*count_dict[t],fillstyle='none')
-    else:
-        ax.plot(psv_within,psv_across,marker=sym,ls='',color=col,label='subject {:s}'.format(sess_names[0][0:2]),markersize=5,fillstyle='none')
-    return psv_within,psv_across
+    if rand_seed is not None: np.random.seed(rand_seed)
+    return np.random.uniform(low=-spacing,high=spacing,size=length)
+
+def vector_angle(v1,v2):
+    # compute angle between 2 vectors
+    import numpy as np
+    import scipy.linalg as slin
+
+    v1, v2 = v1.squeeze(), v2.squeeze()
+    cos_theta = np.dot(v1, v2) / (slin.norm(v1) * slin.norm(v2))
+    theta = np.arccos(np.clip(cos_theta, -1.0, 1.0)) # in radians
+    return theta
 
 # principal angles:
 def prinangle(A, B):
-    import scipy.linalg as slin
     import math
+    import scipy.linalg as slin
     import numpy as np
     # check for 1d vector, ie: shape = (43,)
     if A.ndim == 1:
@@ -156,60 +84,90 @@ def prinangle(A, B):
     pa = list(map(lambda x: math.acos(x) * 180 / math.pi, sv))
     return pa
 
-# function for calculating top angle between global and local dimensions - megan
-def get_top_angles(params_list, metric=None):
-    import numpy as np
+# function for returning the top eigenvector of a matrix
+def get_top_vec(arr,orth=True):
+    # orth tells whether to first orthogonalize the matrix
     import scipy.linalg as slin
-    from utils import prinangle
-    x_angles_list, y_angles_list = [], []
-    sv_wx_list, sv_wy_list, sv_lx_list, sv_ly_list = [], [], [], []
-    for params in params_list:
-        wx = params['W_x']
-        wy = params['W_y']
-        lx = params['L_x']
-        ly = params['L_y']
-        psix = params['psi_x']
-        psiy = params['psi_y']
-
-        wwt_x = wx @ wx.T
-        llt_x = lx @ lx.T
-        wwt_y = wy @ wy.T
-        llt_y = ly @ ly.T
-
-        uwx,swx,_ = slin.svd(wwt_x)
-        ulx,slx,_ = slin.svd(llt_x)
-        uwy,swy,_ = slin.svd(wwt_y)
-        uly,sly,_ = slin.svd(llt_y)
-
-        # top angles
-        pax = prinangle(uwx[:,0], ulx[:,0])
-        x_angles_list.append(pax[0])
-        pay = prinangle(uwy[:,0], uly[:,0])
-        y_angles_list.append(pay[0])
-        
-        # %sv
-        denom_x = np.diag(wwt_x + llt_x) + psix
-        denom_y = np.diag(wwt_y + llt_y) + psiy
-        num_wx, num_wy = (uwx[:,0,None] @ uwx[:,0,None].T)*swx[0], (uwy[:,0,None] @ uwy[:,0,None].T)*swy[0]
-        num_lx, num_ly = (ulx[:,0,None] @ ulx[:,0,None].T)*slx[0], (uly[:,0,None] @ uly[:,0,None].T)*sly[0]
-        sv_wx_list.append(np.mean(np.diag(num_wx) / denom_x) * 100)
-        sv_wy_list.append(np.mean(np.diag(num_wy) / denom_y) * 100)
-        sv_lx_list.append(np.mean(np.diag(num_lx) / denom_x) * 100)
-        sv_ly_list.append(np.mean(np.diag(num_ly) / denom_y) * 100)
-                
-    if metric=="sv":
-        return sv_wx_list, sv_wy_list, sv_lx_list, sv_ly_list
     
-    return x_angles_list, y_angles_list
-
-# function for returning the top eigenvector of a matrix - megan
-def get_top_vec(arr):
-    import scipy.linalg as slin
-    u,_,_ = slin.svd(arr)
+    if orth:
+        u,_,_ = slin.svd(arr)
+    else:
+        u = arr / slin.norm(arr,axis=0)
     top_vec = u[:,0]
     n,_ = arr.shape
     pct_pos = (top_vec >= 0).sum() / n
     if pct_pos < 0.5:
-        return -top_vec, (1-pct_pos)
+        flip = True
+        return -top_vec, (1-pct_pos), flip
     else:
-        return top_vec, pct_pos
+        flip = False
+        return top_vec, pct_pos, flip
+    
+
+# compute top angle between set of pCCA-FA params
+def get_top_angle(params,across_mode='cov'):
+    # params is a parameter dictionary returned by pCCA-FA model
+    # across_mode ('cov' or 'corr') indicates whether to get top covariate mode or correlative mode of W
+    import scipy.linalg as slin
+    import helpers.pcca_fa.pcca_fa_mdl as pf
+
+    mdl = pf.pcca_fa()
+    mdl.set_params(params)
+    wx,wy,lx,ly = mdl.get_loading_matrices()
+
+    if across_mode == 'cov':
+        # orthonormalize W's to be be ordered by SV
+        uwx,_,_ = slin.svd(wx)
+        uwy,_,_ = slin.svd(wy)
+    elif across_mode == 'corr':
+        # order W by canon corrs
+        uwx,uwx = mdl.get_correlative_modes()
+    else:
+        raise ValueError('across_mode must be "cov" or "corr"')
+    
+    # orthogonalize L to be ordered by SV
+    ulx,_,_ = slin.svd(lx)
+    uly,_,_ = slin.svd(ly)
+
+    # top angles
+    x_angle = prinangle(uwx[:,0], ulx[:,0])[0]
+    y_angle = prinangle(uwy[:,0], uly[:,0])[0]
+
+    return x_angle, y_angle
+
+# for cross-validation - determine acceptable latent dimensionalities to test
+def get_zlists(X,Y,cca_dim,fa_dim):
+    # X and Y are spike trains (xDim x trials)
+    # cca_dim and fa_dim are desired maximum dimensionality to test
+    import numpy as np
+    max_dim = np.minimum(X.shape[1],Y.shape[1]) # max acceptable dim relative to number of neurons
+    zDim = np.minimum(cca_dim,max_dim)
+    zxDim = np.minimum(fa_dim,X.shape[1])
+    zyDim = np.minimum(fa_dim,Y.shape[1])
+    return (np.arange(zDim)+1).astype(int),(np.arange(zxDim)+1).astype(int),(np.arange(zyDim)+1).astype(int)
+
+# generate Gaussian Process
+def gen_GP(GP_tau, GP_len, noise_var=1e-3, seed=0, N=1):
+    import numpy as np
+    import scipy.linalg as slin
+    from scipy.spatial.distance import pdist, squareform
+    # generate GP covariance matrix
+    t = np.expand_dims(np.arange(GP_len),1)
+    dists = squareform(pdist(t))
+    K = (1-noise_var)*np.exp( -np.square(dists) / (2*GP_tau**2)) + np.eye(GP_len)*noise_var
+    sqrt_K = np.real(slin.sqrtm(K))
+    
+    # set random seed
+    np.random.seed(seed)
+    
+    # generate GP
+    GPs = np.zeros((GP_len,N))
+    for i in range(N):
+        GPs[:,i] = sqrt_K.dot(np.random.randn(GP_len))
+    return GPs.squeeze()
+
+def nansem(data,axis=0):
+    # compute standard error of the mean, ignoring NaN values
+    import numpy as np
+    sem = np.nanstd(data,axis=axis) / np.sqrt(np.count_nonzero(~np.isnan(data),axis=axis))
+    return sem
