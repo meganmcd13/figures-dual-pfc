6678aca285d72c6229205e7cc6c70cac968f52e9
meganmcd13 <mmcdonne@andrew.cmu.edu>
2025-09-09 17:34:08 -0400

    refactor notation for fig5 chance

--- create_fig5_chance.py
+++ create_fig5_chance.py
@@ -12,7 +12,7 @@ import sys
 import numpy as np
 import scipy.io as sio
 
-sys.path.append('helpers/')
+sys.path.append('helpers/pcca_fa/')
 from dual_pfc_funcs import get_top_angle, getParams, save_dict, load_dict
 import helpers.pcca_fa.pcca_fa_mdl as pf
 import helpers.pcca_fa.sim_pcca_fa as spf
@@ -49,30 +49,30 @@ for subject in subjects:
         LH = getattr(curr_dat,'fast_component_left')
         RH = getattr(curr_dat,'fast_component_right')
         n_trials = curr_dat.n_trials
-        xDim,yDim = LH.shape[1],RH.shape[1]
+        n1,n2 = LH.shape[1],RH.shape[1]
 
         # get pCCA-FA params
         params = pccafa_fits[sess]['params'].copy()
-        zDim,zxDim,zyDim = params['zDim'],params['zxDim'],params['zyDim']
-        x_angle_gt, y_angle_gt = get_top_angle(params.copy(),across_mode='cov')
-        results[sess]['neural_x'] = x_angle_gt
-        results[sess]['neural_y'] = y_angle_gt
+        d,d1,d2 = params['d'],params['d1'],params['d2']
+        x1_angle_gt, x2_angle_gt = get_top_angle(params.copy(),across_mode='cov')
+        results[sess]['neural_x1'] = x1_angle_gt
+        results[sess]['neural_x2'] = x2_angle_gt
 
         mdl = pf.pcca_fa()
         mdl.set_params(params.copy())
         psv_gt = mdl.compute_psv()
-        shared_within_x_orig = np.diag(params['L_x'] @ params['L_x'].T)
+        shared_within_x1_orig = np.diag(params['L_1'] @ params['L_1'].T)
     
         # simulator
-        simulator = spf.sim_pcca_fa(xDim,yDim,zDim,zxDim,zyDim)
+        simulator = spf.sim_pcca_fa(n1,n2,d,d1,d2)
         simulator.set_params(params.copy())
-        tmp_x,tmp_y = get_top_angle(simulator.get_params())
-        assert(np.isclose(tmp_x,x_angle_gt,rtol=1e-5) and np.isclose(tmp_y,y_angle_gt,rtol=1e-5))
+        tmp_x1,tmp_x2 = get_top_angle(simulator.get_params())
+        assert(np.isclose(tmp_x1,x1_angle_gt,rtol=1e-5) and np.isclose(tmp_x2,x2_angle_gt,rtol=1e-5))
 
         #########################################
         # option 1: adjust L to have random theta
-        print('   manipulating hem X')
-        simulator.apply_rotation(thetas_rand[i_sess,0],hem='x')
+        print('   manipulating area 1')
+        simulator.apply_rotation(thetas_rand[i_sess,0],hem='1')
         tmp,_ = get_top_angle(simulator.get_params())
         assert(np.isclose(tmp,thetas_rand[i_sess,0],rtol=1e-5))
 
@@ -80,80 +80,80 @@ for subject in subjects:
         mdl = pf.pcca_fa()
         mdl.set_params(new_params)
         psv_rot = mdl.compute_psv()
-        shared_within_x_new = np.diag(new_params['L_x'] @ new_params['L_x'].T)
-        print('  %sv across: {:.2f}, %sv within: {:.2f}'.format(psv_gt['psv_x'], psv_gt['psv_priv_x']))
-        print('  ROTATED %sv across: {:.2f}, %sv within: {:.2f}'.format(psv_rot['psv_x'], psv_rot['psv_priv_x']))
+        shared_within_x1_new = np.diag(new_params['L_1'] @ new_params['L_1'].T)
+        print('  %sv across: {:.2f}, %sv within: {:.2f}'.format(psv_gt['avg_psv_W_1'], psv_gt['avg_psv_L_1']))
+        print('  ROTATED %sv across: {:.2f}, %sv within: {:.2f}'.format(psv_rot['avg_psv_W_1'], psv_rot['avg_psv_L_1']))
 
-        X,Y = simulator.sim_data(n_trials)
+        X_1,X_2 = simulator.sim_data(n_trials)
     
         # fit the data using pcca-fa
         model = pf.pcca_fa()
-        model.train(X,Y,zDim,zxDim,zyDim,warmstart=False)
+        model.train(X_1,X_2,d,d1,d2,warmstart=False)
         fit_params = model.get_params()
     
         # find estimated angles for hemisphere X
-        x_angle,_ = get_top_angle(fit_params)
-        results[sess]['gt_rand_x'] = thetas_rand[i_sess,0]
-        results[sess]['fit_rand_x'] = x_angle
+        x1_angle,_ = get_top_angle(fit_params)
+        results[sess]['gt_rand_x1'] = thetas_rand[i_sess,0]
+        results[sess]['fit_rand_x1'] = x1_angle
 
-        print('   manipulating hem Y')
+        print('   manipulating area 2')
         simulator.set_params(params.copy())
-        tmp_x,tmp_y = get_top_angle(simulator.get_params())
-        assert(np.isclose(tmp_x,x_angle_gt,rtol=1e-5) and np.isclose(tmp_y,y_angle_gt,rtol=1e-5))
-        simulator.apply_rotation(thetas_rand[i_sess,1],hem='y')
+        tmp_x1,tmp_x2 = get_top_angle(simulator.get_params())
+        assert(np.isclose(tmp_x1,x1_angle_gt,rtol=1e-5) and np.isclose(tmp_x2,x2_angle_gt,rtol=1e-5))
+        simulator.apply_rotation(thetas_rand[i_sess,1],hem='2')
         _,tmp = get_top_angle(simulator.get_params())
         assert(np.isclose(tmp,thetas_rand[i_sess,1],rtol=1e-5))
-        X,Y = simulator.sim_data(n_trials)
+        X_1,X_2 = simulator.sim_data(n_trials)
     
         # fit the data using pcca-fa
         model = pf.pcca_fa()
-        model.train(X,Y,zDim,zxDim,zyDim,warmstart=False)
+        model.train(X_1,X_2,d,d1,d2,warmstart=False)
         fit_params = model.get_params()
     
         # find estimated angles for hemisphere Y
-        _, y_angle = get_top_angle(fit_params)
-        results[sess]['gt_rand_y'] = thetas_rand[i_sess,1]
-        results[sess]['fit_rand_y'] = y_angle
+        _, x2_angle = get_top_angle(fit_params)
+        results[sess]['gt_rand_x2'] = thetas_rand[i_sess,1]
+        results[sess]['fit_rand_x2'] = x2_angle
 
         #############################################
         # option 2: adjust L to have structured theta
         print('   manipulating structured thetas')
         for k,theta in enumerate(thetas_struct):
-            # hemisphere 1: manipulate X
+            # manipulate area 1
             simulator.set_params(params.copy())
-            tmp_x,tmp_y = get_top_angle(simulator.get_params())
-            assert(np.isclose(tmp_x,x_angle_gt,rtol=1e-5) and np.isclose(tmp_y,y_angle_gt,rtol=1e-5))
-            simulator.apply_rotation(theta,hem='x')
+            tmp_x1,tmp_x2 = get_top_angle(simulator.get_params())
+            assert(np.isclose(tmp_x1,x1_angle_gt,rtol=1e-5) and np.isclose(tmp_x2,x2_angle_gt,rtol=1e-5))
+            simulator.apply_rotation(theta,hem='1')
             tmp,_ = get_top_angle(simulator.get_params())
             assert(np.isclose(tmp,theta,rtol=1e-5))
-            X,Y = simulator.sim_data(n_trials)
+            X_1,X_2 = simulator.sim_data(n_trials)
 
             # fit the data using pcca-fa
             model = pf.pcca_fa()
-            model.train(X,Y,zDim,zxDim,zyDim,warmstart=False)
+            model.train(X_1,X_2,d,d1,d2,warmstart=False)
             fit_params = model.get_params()
 
-            # find estimated angles for hemisphere X
-            x_angle, _ = get_top_angle(fit_params)
-            results[sess]['fit_theta{}_x'.format(theta)] = x_angle
+            # find estimated angles for area 1
+            x1_angle, _ = get_top_angle(fit_params)
+            results[sess]['fit_theta{}_x1'.format(theta)] = x1_angle
 
-            # hemisphere 2: manipulate Y
+            # manipulate area 2
             simulator.set_params(params.copy())
-            tmp_x,tmp_y = get_top_angle(simulator.get_params())
-            assert(np.isclose(tmp_x,x_angle_gt,rtol=1e-5) and np.isclose(tmp_y,y_angle_gt,rtol=1e-5))
-            simulator.apply_rotation(theta,hem='y')
+            tmp_x1,tmp_x2 = get_top_angle(simulator.get_params())
+            assert(np.isclose(tmp_x1,x1_angle_gt,rtol=1e-5) and np.isclose(tmp_x2,x2_angle_gt,rtol=1e-5))
+            simulator.apply_rotation(theta,hem='2')
             _,tmp = get_top_angle(simulator.get_params())
             assert(np.isclose(tmp,theta,rtol=1e-5))
-            X,Y = simulator.sim_data(n_trials)
+            X_1,X_2 = simulator.sim_data(n_trials)
 
             # fit the data using pcca-fa
             model = pf.pcca_fa()
-            model.train(X,Y,zDim,zxDim,zyDim,warmstart=False)
+            model.train(X_1,X_2,d,d1,d2,warmstart=False)
             fit_params = model.get_params()
 
             # find estimated angles
-            _, y_angle = get_top_angle(fit_params)
-            results[sess]['fit_theta{}_y'.format(theta)] = y_angle
+            _, x2_angle = get_top_angle(fit_params)
+            results[sess]['fit_theta{}_x2'.format(theta)] = x2_angle
 
         # save results each session
         save_dict(results,save_name)
