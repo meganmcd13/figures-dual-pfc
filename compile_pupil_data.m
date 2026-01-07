d2cc91b2d7099322c1f8e292cd5271f5d3109acd
meganmcd13 <mmcdonne@andrew.cmu.edu>
2026-01-06 19:53:50 -0500

    update pupil measurements windows

--- compile_pupil_data.m
+++ compile_pupil_data.m
@@ -12,8 +12,14 @@ save_dir = 'preprocessed_data/';
 % useful trial codes - Smith lab
 fixon_code = 2; targon_code = 70; targoff_code = 100; fixoff_code = 3;
 ar_order = 25; % length of trial window to subtract slow component
-for sub=subjects
-    sub_str = sub{1};
+
+% manual selection of peak of evoked response
+peaks = {[150, 127, 161, 181, 165, 149, 154, 129, 161, 170, 177, 175, 200, 166, 178, 150], ... % satchel
+         [330, 356, 347, 351, 306, 297, 319, 276, 285, 281], ...                               % wakko
+         [297, 280, 297, 224, 221, 269, 294, 290, 293, 287, 275, 264, 252, 288, 276, 273]};    % pepe
+
+for i_sub=1:length(subjects)
+    sub_str = subjects{i_sub};
 
     % get all data 
     load(sprintf('%s/all_data_delay_%s.mat',save_dir,sub_str),'all_data');
@@ -36,16 +42,20 @@ for sub=subjects
         n_samp = nan(1,N);
         for jj = 1:N
             codes = curr_pupil(jj).codesamples;
-            start_idx = codes(find(codes(:,1)==targon_code,1),2) - 200;
+            start_idx = codes(find(codes(:,1)==targon_code,1),2) - PRE_TO;
+            delay_start_idx = codes(find(codes(:,1)==targoff_code,1),2);
             end_idx = codes(find(codes(:,1)==fixoff_code,1),2);
             smoothed_trace = unpadded_medfilt1(double(curr_pupil(jj).trial((start_idx+1):end_idx)),50); % remove high-frequency noise from signal
+            delay_smoothed_trace = unpadded_medfilt1(double(curr_pupil(jj).trial((delay_start_idx+1):end_idx)),50); % remove high-frequency noise from signal
 
             trial{jj} = smoothed_trace;
-            avg_outlier(jj) = trimmean(smoothed_trace,20); % remove outlier values and compute average over the delay
-            avg(jj) = mean(smoothed_trace); % compute average over the delay, including outlier points
-            baseline(jj) = mean(smoothed_trace(51:150)); % -150 to -50 pre target onset
-            evoked(jj) = mean(smoothed_trace(251:350)); % +50 to +150 post target onset
+            avg(jj) = mean(delay_smoothed_trace); % compute average over the delay, including outlier points
+            baseline(jj) = mean(smoothed_trace(PRE_TO-200+1:PRE_TO-100)); % -200 to -100 pre target onset
             n_samp(jj) = length(smoothed_trace); % number of pupil samples in the delay period
+
+            % find evoked response magnitude
+            evoked_start = PRE_TO + peaks{i_sub}(ii);
+            evoked(jj) = mean(smoothed_trace(evoked_start-49:evoked_start+50)); % -50 to +50 centered on manual peak of evoked response
         end
         pupil.trial = trial;
         pupil.avg = avg;
